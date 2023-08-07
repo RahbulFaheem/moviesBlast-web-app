4722c3a397e0b2f267b6f6f1c07dc7d3f48f3edc
RahbulFaheem <rahbulfaheem5@gmail.com>
2023-08-07 11:53:49 +0500

    Movie-Blast Web App

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    FaLinkedin,
+    FaGithub
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import "./style.scss";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper>
+
+                <div className="socialIcons">
+                    <a  href="https://github.com/RahbulFaheem" target="_blank">
+                        <span className="icon">
+                            <FaGithub />
+                        </span>
+                    </a>    
+                    <a  href="https://www.linkedin.com/in/rahbulfaheem/" target="_blank">
+                        <span className="icon">
+                        <FaLinkedin />
+                        </span>
+                    </a>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
